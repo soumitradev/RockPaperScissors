29a94fb8d3999ff9113ad3130806332fec377a84
soumitradev <soumitra.shewale.dev@gmail.com>
2020-01-19 10:07:02 +0530

    Update game
    Points added, and best of 3.

--- README.md
+++ README.md
@@ -6,10 +6,10 @@
 
 ## How to Play
 Just enter "Rock", "Paper" or "Scissors" at the prompt! (Input is NOT case sensitive)
+Game is best of 3. If game is a tie, you'll have to play again.
 
 ## Why OOP?
-It is very easy to create derivatives such as Rock Paper Scissors Lizard Spock, etc! It does not include weird repeated
-or nested ternary operators.
+It is very easy to create derivatives such as Rock Paper Scissors Lizard Spock, etc!
 
 ## Why ternary operators instead of `if`s?
 Why not?
--- RockPaperScissors.jl
+++ RockPaperScissors.jl
@@ -39,7 +39,12 @@ function get_input(prompt)
     ((length(txt) > 1) || !(txt[1] in ["rock", "scissors", "paper"])) ? get_input(prompt) : return txt[1]
 end
 
-while true
+# Create points for players
+AI_points = 0
+human_points = 0
+
+while (AI_points + human_points) < 3
+    global AI_points, human_points
     # Ask for input and get move by user
     player1_move = to_thing(get_input("Enter your move (Rock, Paper or Scissors): "))
 
@@ -52,10 +57,18 @@ while true
     # Check winner
     result = check_winner(player1_move, player2_move)
 
-    # Print result
-    (result == 1) ? println("You win") : (result == 2) ? println("AI wins") : println("Tie.")
-    break
+    # Print result. If tie, then game continues, since game counter is sum of AI_points and human_points, the game goes on one more time
+    (result == 1) ? (println("You win"); human_points += 1) : (result == 2) ? (println("AI wins"); AI_points += 1) : (println("Tie. No one gets a point. Play again.\n"); continue)
+    
+    # Print current standings
+    println("Current points - You: $human_points, AI: $AI_points\n")
+
+    # If any player gets 2 points out of 3, playing game again is pointless, so let the player with 2 points win.
+    max(AI_points, human_points) == 2 ? break : ""
 end
 
-# Ask if user want to play again
+# Print game result
+(AI_points > human_points) ? println("\nGAME! AI Wins.") : println("\nGAME! You Win.")
+
+# Ask if user wants to play again
 play_again()
